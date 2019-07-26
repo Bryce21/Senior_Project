add023a7c0fedddc36fe621362f6f4059cf241d1
Bryce Reinhard <reinhard.bryce.21@gmail.com>
2019-07-25 21:37:05 -0400

    Some stuff done

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Konva_Wrapper from './konva_wrapper'
 import Info from './info.js'
-import * as algos from '../algos.js'
 
 
 export default class Main extends React.Component {
@@ -20,7 +19,8 @@ export default class Main extends React.Component {
                 width: 0,
                 height: 0
             },
-            algoSelectorValue: 'Guillotine'
+            algoSelectorValue: 'Guillotine',
+            presort_descending: false
 
         };
     }
@@ -61,33 +61,44 @@ export default class Main extends React.Component {
     renderRectangleInput() {
         return <div>
             <style>{'body { background-color: white; }'}</style>
-            <div class="text-monospace p-3 mb-2 bg-warning text-dark text-left font-weight-bolder">Rectangle(s) Inputs</div>
-            <div class="text-monospace p-3 mb-2 bg-warning text-dark text-left font-weight-bolder">Length, Height, Quantity:</div>
 
             {this.state.rectangles.map((rectangle, idx) => (
                 <div className="rectangle">
-                    <box><input
-                        type="number"
-                        min="0"
-                        value={rectangle.length}
-                        name={"length"}
-                        onChange={this.handleRectangleNameChange(idx)}
-                    /></box>
-                    <input
-                        type="number"
-                        min="0"
-                        value={rectangle.height}
-                        name={"height"}
-                        onChange={this.handleRectangleNameChange(idx)}
-                    />
+                    <label>
+                        Length:
+                        <input
+                            type="number"
+                            min="0"
+                            value={rectangle.length}
+                            name={"length"}
+                            onChange={this.handleRectangleNameChange(idx)}
+                        />
+                    </label>
 
-                    <box><input
-                        type="number"
-                        min="0"
-                        value={rectangle.quantity}
-                        name={"quantity"}
-                        onChange={this.handleRectangleNameChange(idx)}
-                    /></box>
+
+                    <label>
+                        Height:
+                        <input
+                            type="number"
+                            min="0"
+                            value={rectangle.height}
+                            name={"height"}
+                            onChange={this.handleRectangleNameChange(idx)}
+                        />
+                    </label>
+
+                    <label>
+                        Quantity:
+                        <input
+                            type="number"
+                            min="0"
+                            value={rectangle.quantity}
+                            name={"quantity"}
+                            onChange={this.handleRectangleNameChange(idx)}
+                        />
+                    </label>
+
+
 
                     <button
                         type="button"
@@ -116,18 +127,18 @@ export default class Main extends React.Component {
             <div>
                 
                 <form onSubmit={this.handleSubmit}>
-                    <div class="text-monospace p-3 mb-2 bg-warning text-dark text-center font-weight-bolder">Material
-                        Site Application
-                    </div>
-                    <div class="text-monospace p-3 mb-2 bg-warning text-dark text-left font-weight-bolder">
+                    <div class="text-monospace p-3 mb-2 bg-warning text-dark text-center font-weight-bolder">Material Site Application</div>
+
+                    <label>
                         Area to fill length:
                         <input type="text" min="0" name="area_length" onChange={this.handleChangeAbstract}/>
-                    </div>
+                    </label>
 
-                    <div class="text-monospace p-3 mb-2 bg-warning text-dark text-left font-weight-bolder">
-                        Area to fill height:
+                    <label>
+                        Area to fill length:
                         <input type="text" min="0" name="area_height" onChange={this.handleChangeAbstract}/>
-                    </div>
+                    </label>
+
 
                     <label>
                         API level:
@@ -138,7 +149,23 @@ export default class Main extends React.Component {
                             <option value="GuillotineBAF">Guillotine Best Area</option>
                         </select>
                     </label>
+
+                    <label>
+                        Pre-sort descending:
+                        <input
+                            name="presort_descending"
+                            type="checkbox"
+                            checked={this.state.presort_descending}
+                            onChange={this.handleInputChange}
+                        />
+                    </label>
+
+                    <hr/>
+
                     {this.renderRectangleInput()}
+
+                    <hr/>
+
                     <button
                         type="button"
                         onClick={this.handleAddRectangle}
@@ -156,52 +183,40 @@ export default class Main extends React.Component {
 
     }
 
+    handleInputChange = (event) =>  {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
 
     handleSubmit = async e => {
         e.preventDefault();
         console.log('Handle submit called');
-        let rectangles_to_use = []
-        for(let i = 0; i < this.state.rectangles.length; i++){
-            for(let x=0; x < parseInt(this.state.rectangles[i].quantity); x++){
-                rectangles_to_use.push(this.state.rectangles[i])
-            }
-        }
 
-        let body = {
-            area_length: this.state.area_length,
-            area_height: this.state.area_height,
-            rectangles: rectangles_to_use,
-            algoSelectorValue: this.state.algoSelectorValue
-        };
-        console.log(body)
-
-
-        console.log(typeof(body))
-        // switch(body.algoSelectorValue){
-        //     case(''): break;
-        //     case(''): break;
-        //     case(''): break
-        // }
-        let points = algos.guillotine(body.rectangles, body.area_length, body.area_height);
-        console.log(points)
-        console.log(typeof(points))
-        this.setState({points: points})
-        // await fetch('/api/algorithms', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: body,
-        // }).then((response) => {
-        //     response.text().then((text) => {
-        //         console.log(text);
-        //         let data = JSON.parse(text);
-        //         this.setState({points: data})
-        //     });
-        //
-        // });
-
-
+        await fetch('/api/algorithms', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                area_length: this.state.area_length,
+                area_height: this.state.area_height,
+                rectangles: this.state.rectangles,
+                algoSelectorValue: this.state.algoSelectorValue,
+                presort_descending: this.state.presort_descending
+            }),
+        }).then((response) => {
+            response.text().then((text) => {
+                console.log(text);
+                let data = JSON.parse(text);
+                this.setState({points: data})
+            });
+        });
     };
 }
 
